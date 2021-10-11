683697d6ef09bf8e5124d270ef97b04fd99bbdae
powerLMZ <92151493+powerLMZ@users.noreply.github.com>
2021-10-11 15:54:18 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
-  someSidebar: {
+  docs: [
+    'Home'
   {
       type: 'category',
       label: '硬件设计 - 分立元件',
@@ -7,5 +8,5 @@ module.exports = {
     '基本元器件-电阻',
       ],
     },
-  },
+  ],
 };
