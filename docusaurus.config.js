c03e1c20729f49dd7412c35a201701b937c5a945
powerLMZ <92151493+powerLMZ@users.noreply.github.com>
2021-10-13 20:35:25 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,11 +38,11 @@ module.exports = {
       
       // 导航栏上的按钮  按照相应的格式可以创建新的按钮
       items: [
-        {
-          to: "blog",                   // 要跳转的页面
-          label: '博客',                // 按钮名称
-          position: 'left'              // 按钮位于左边还是右边
-        },
+  //      {
+   //       to: "blog",                   // 要跳转的页面
+    //      label: '博客',                // 按钮名称
+    //      position: 'left'              // 按钮位于左边还是右边
+   //     },
         
         {
           to: 'docs/',
