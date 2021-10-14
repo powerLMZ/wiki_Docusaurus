960237ae7e668e497e61d08878140d66f758fed5
powerLMZ <92151493+powerLMZ@users.noreply.github.com>
2021-10-14 13:58:31 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,20 +15,33 @@ module.exports = {
   
   themeConfig: {
 
-    // 配置搜索栏需要到 algolia 配置
-    // algolia: {
-    //   apiKey: 'YOUR_API_KEY',
-    //   indexName: 'YOUR_INDEX_NAME',
+    image: 'https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-zip.png',
+    algolia: {
+      apiKey: "5c07d8bf9c9928c4453857f6cad0420e",
+      indexName: "power",
+      contextualSearch: true,
+      searchParameters: {},
+    },
+      colorMode: {
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
 
-    //   // Optional: see doc section bellow
-    //   contextualSearch: true,
+      switchConfig: {
+       
+        darkIcon: '🌙',
+        lightIcon: '🌞',
 
-    //   // Optional: Algolia search parameters
-    //   searchParameters: {},
+        darkIconStyle: {
+          marginLeft: "2px",
+        },
 
-    //   //... other Algolia params
-    // },
-    // 导航栏配置
+        lightIconStyle: {
+          marginLeft: "1px",
+        },
+      },
+    },
+
+    hideableSidebar: false,
     navbar: {
       title: "Lmz's Site",                 // 名称
       hideOnScroll: false,
@@ -41,47 +54,49 @@ module.exports = {
       // 导航栏上的按钮  按照相应的格式可以创建新的按钮
       items: [
         {
-          to: "docs/",                   // 要跳转的页面
-          label: '博客',                // 按钮名称
-          position: 'left'              // 按钮位于左边还是右边
+          to: "blog",                  // 要跳转的页面
+          label: "博客",               // 按钮名称
+          position: "left",              // 按钮位于左边还是右边
         },
-        
-    //    {
-    //     to: 'docs/',
-    //      activeBasePath: 'docs',
-    //      label: 'Docs',
-    //      position: 'left',
-    //    },
-        
         {
           href: 'https://github.com/rcxxx/docusaurus-template',  // 如果要跳转链接则使用 herf
           label: '本站源码',
-          position: 'right',
+          position: 'left',
         },
       ],
     },
 
     // 页脚配置
-    footer: {
-      style: 'dark',                    // 页脚风格
-      links: [
+    //footer: {
+    //  style: 'dark',                    // 页脚风格
+     // links: [
 
         // 同样的方式创建一个分类
-        {
-          title: 'Docs',
+     //   {
+      //    title: 'Docs',
           // 同样的格式创建新的按钮
-          items: [
-            {
-              label: 'start',           // 标签
-              to: 'docs/',              // 要跳转的页面
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
+      //    items: [
+       //     {
+       //       label: 'start',           // 标签
+       //       to: 'docs/',              // 要跳转的页面
+       //     },
+       //   ],
+      //  },
+     // ],
+   //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+   // },
+//  },
   
+      stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/gh/linyuxuanlin/Wiki_Docusaurus/static/katex/v0.12.0/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
+      crossorigin: 'anonymous',
+    },
+  ],
+    
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -97,17 +112,23 @@ module.exports = {
           rehypePlugins: [katex],
         },
         blog: {
-          showReadingTime: true,
+          
+          blogSidebarCount: 8,
+          postsPerPage: 8,
+          showReadingTime: false,
           path: "./blog",
-          routeBasePath: "/",           // 这里将 blog/ 设置为首页
+          blogSidebarTitle: 'Recent',
+          editUrl: "https://github.com/powerLMZ/wiki_Docusaurus/edit/main/",
+         // routeBasePath: "/",           // 这里将 blog/ 设置为首页
         },
-        docs: {
+   /*     docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           
           // 修改为自己的链接，在文章底部添加编辑此页面的链接
           // editUrl:
           //   'https://github.com/facebook/docusaurus/edit/master/website/',
         },
+        */
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
