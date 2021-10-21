4229ee69c8289bf97ab2a0c016ca16b443a17838
powerLMZ <92151493+powerLMZ@users.noreply.github.com>
2021-10-21 10:36:25 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,69 @@
-const math = require('remark-math');const katex = require('rehype-katex');
-
 module.exports = {
   title: "LMZ's Site",              // 站点名称
   tagline: 'Learning notes',  // 站点描述
   url: "https://wiki-docusaurus-orpin.vercel.app/docs/",
   baseUrl: '/',                         // 项目的基本 URL 设置为 /仓库名/
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',           // 站点的图标
+  organizationName: 'lmz',
+  projectName: 'lmz-docusaurus2',
+  //onBrokenMarkdownLinks: 'warn',
   
+themeConfig: {
   hideableSidebar: false,
-
     navbar: {                           //导航栏
       title: "Lmz's Site",                 // 名称
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
       hideOnScroll: false,
       items: [
-       /* {
-          to: "docs/home",                  // 要跳转的页面
-          label: "wiki",               // 按钮名称
-          position: "left",              // 按钮位于左边还是右边
-        },*/
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},        
         {
           href: 'https://github.com/rcxxx/docusaurus-template',  // 如果要跳转链接则使用 herf
-          label: '本站源码',
-          position: 'left',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
-  
-    /*  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/gh/linyuxuanlin/Wiki_Docusaurus/static/katex/v0.12.0/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
-      crossorigin: 'anonymous',
-    },
-  ],*/
-    
+  //My site的页脚
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'home',
+              to: 'docs/home',
+            },
+            {
+              label: '基本元器件-电阻',
+              to: 'docs/基本元器件-电阻/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+     },
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -50,23 +76,18 @@ module.exports = {
           showLastUpdateAuthor: false,
           showLastUpdateTime: false,
           editUrl: "https://github.com/powerLMZ/wiki_Docusaurus/edit/main/",
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
+               },
         blog: {
-          
           blogSidebarCount: 8,
           postsPerPage: 8,
           showReadingTime: false,
           path: "./blog",
           blogSidebarTitle: 'Recent',
           editUrl: "https://github.com/powerLMZ/wiki_Docusaurus/edit/main/",
-
-        },
-        
+               },
         theme: {
          customCss: require.resolve('./src/css/custom.css'),
-        },
+               },
       },
     ],
   ],
